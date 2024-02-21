bcdaa6a7d32f24f1ee16b661b2d82d2312984821
Caleb-Abuul <abuulcaleb@gmail.com>
2024-02-21 13:12:04 +0100

    implemented form navigation

--- main.js
+++ main.js
@@ -1,25 +1,59 @@
-let grabId = (id) => document.getElementById(id);
+const grabItem = (id) => document.getElementById(id);
 
-let fullName = grabId('name'), email = grabId('email'), phoneNumber = grabId('phone'), btn = grabId('btn');
-let currentFormIndex = 0;
-let currentNumberIndex = 0;
-let formFields = ['step1Form', 'step2Form', 'step3Form', 'step4Form', 'step5'];
-let numbers = [1, 2, 3, 4];
+let fullName = grabItem('name'), email = grabItem('email'), phoneNumber = grabItem('phone'), btn = grabItem('btn');
+let prevBtn = grabItem('prevBtn');
 
-function nextForm(){
-    let currentFormField = grabId(formFields[currentFormIndex]);
-    let currentNumberItem = grabId(numbers[currentNumberIndex]);
-    currentFormIndex++;
-    currentNumberIndex++;
-    let nextFormField = grabId(formFields[currentFormIndex]);
-    let nextNumberItem = grabId(numbers[currentNumberIndex]);
-    
-    currentFormField.style.display = 'none';
-    nextFormField.style.display = 'block';
-}
+let form2Title = 'Select your plan';
+let form2description = 'You have the option of monthly or yearly billing.';
 
+let form3Title = 'Pick add-ons';
+let form3description = 'Add-ons help enhance your gaming experience.';
+
+let form4Title = 'Finishing up';
+let form4description = 'Double-check everything looks OK before confirming.';
+
+let elementIndex = 0;
 btn.addEventListener('click', (e)=>{
     e.preventDefault();
+    setElementIndex();
 
-    nextForm();
-});
+    let elementIds = [];
+    for (let i = 0; i < 4; i++){
+        elementIds.push(grabItem(i));
+    }
+    console.log(elementIds);
+    
+    elementIds[elementIndex].style.display = 'block';
+    elementIds[elementIndex - 1].style.display = 'none';
+    console.log(elementIndex);
+})
+
+function setElementIndex(){
+    if (elementIndex == 5){
+        return;
+    }
+    elementIndex++;
+}
+
+prevBtn.addEventListener('click', (e)=>{
+    e.preventDefault();
+
+    let elementIds = [];
+    for (let i = 0; i < 4; i++){
+        elementIds.push(grabItem(i));
+    }
+    console.log(elementIndex);
+    
+    elementIds[elementIndex - 1].style.display = 'block';
+    elementIds[elementIndex].style.display = 'none';
+    decreaseElemtentIdex();
+})
+
+function decreaseElemtentIdex(){
+    if (elementIndex == -1){
+        return;
+    }
+    elementIndex--;
+}
+
+console.log('hey');
